f61442a21d4aef23cc250661c61c7ee3a4e92dfb
Chenxin Yan <yanchenxin2004@gmail.com>
2025-09-29 14:13:15 -0400

    refactor schames to modules for validation reuse

---
+++ packages/server/convex/schemas/courses.ts
@@ -0,0 +1,31 @@
+import { v } from "convex/values";
+
+const courses = {
+  code: v.string(), // CSCI-UA 101
+  level: v.string(), // 100
+  title: v.string(), // Intro to Computer Science
+  credits: v.int64(), // 4
+  description: v.string(),
+  courseUrl: v.string(),
+};
+
+const prerequisites = v.union(
+  v.object({
+    courseId: v.id("courses"),
+    type: v.literal("required"),
+    courses: v.array(v.string()), // course code
+  }),
+  v.object({
+    courseId: v.id("courses"),
+    type: v.literal("alternative"),
+    courses: v.array(v.string()), // course code
+  }),
+  v.object({
+    courseId: v.id("courses"),
+    type: v.literal("options"),
+    courses: v.array(v.string()), // course code
+    creditsRequired: v.int64(),
+  }),
+);
+
+export { courses, prerequisites };
