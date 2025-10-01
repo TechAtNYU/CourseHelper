461d65e91ac60bb456f1897e60c9a00cf7cbe7ea
Michael Wang <mzw2010@nyu.edu>
2025-10-01 14:42:54 -0400

    added shadcn default dependencies

---
+++ apps/web/src/components/DashboardSidebar.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { useAuth } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import type { ReactNode } from "react";
+import { env } from "@/lib/env";
+
+
