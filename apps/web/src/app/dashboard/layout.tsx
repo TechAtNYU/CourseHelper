5c541ab0583a58d6c29a516500932b1f2b666fc5
Michael Wang <mzw2010@nyu.edu>
2025-10-01 15:32:58 -0400

    default shadcn mobile hook and added generic sidebar

---
+++ apps/web/src/app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+// import { auth } from "@clerk/nextjs/server";
+// import { redirect } from "next/navigation";
+
+import { DashboardSidebar } from "@/components/DashboardSidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <DashboardSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+}
--- apps/web/src/components/DashboardSidebar.tsx
+++ apps/web/src/components/DashboardSidebar.tsx
@@ -1,9 +1,79 @@
 "use client";
 
-import { useAuth } from "@clerk/nextjs";
-import { ConvexReactClient } from "convex/react";
-import { ConvexProviderWithClerk } from "convex/react-clerk";
-import type { ReactNode } from "react";
-import { env } from "@/lib/env";
+// import { useAuth } from "@clerk/nextjs";
+// import { ConvexReactClient } from "convex/react";
+// import { ConvexProviderWithClerk } from "convex/react-clerk";
+// import type { ReactNode } from "react";
+// import { env } from "@/lib/env";
 
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
 
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const menuItems = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+];
+
+const footer = {
+  title: "Settings",
+  url: "#",
+  icon: Settings,
+};
+
+export function DashboardSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Course Helper</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {menuItems.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
