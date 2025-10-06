b90248b97fccbc0ac8b5c90be074fa3063d2d6b5
Emily Silkina <emily.silkin@gmail.com>
2025-10-06 19:37:37 -0400

    Created schedule page

---
+++ apps/web/src/app/dashboard/schedule/page.tsx
@@ -0,0 +1,14 @@
+// import { AppHeader } from "./components/app-header";
+
+const SchedulePage = () => {
+  return (
+    <>
+      {/* <AppHeader title="Dashboard" /> */}
+      <main>
+        <h1>Calendar</h1>
+      </main>
+    </>
+  );
+};
+
+export default SchedulePage;
