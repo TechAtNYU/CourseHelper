fe5f16faa812cccdb111a6a8621d99b4f20f6bec
Chenxin Yan <yanchenxin2004@gmail.com>
2025-10-07 00:23:43 -0400

    add plan page

---
+++ apps/web/src/app/dashboard/plan/page.tsx
@@ -0,0 +1,13 @@
+import FourYearPlan from "@/components/FourYearPlan/FourYearPlan";
+import { AppHeader } from "../components/app-header";
+
+const PlanPage = () => {
+  return (
+    <>
+      <AppHeader title="Four Year Plan" />
+      <FourYearPlan />
+    </>
+  );
+};
+
+export default PlanPage;
