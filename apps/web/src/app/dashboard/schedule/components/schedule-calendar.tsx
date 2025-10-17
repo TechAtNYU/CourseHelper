8c5ec32586e23d6408fd01133f81efff7dfc83bc
Chenxin Yan <yanchenxin2004@gmail.com>
2025-10-17 16:47:33 -0400

    remove loading state

--- apps/web/src/app/dashboard/schedule/components/schedule-calendar.tsx
+++ apps/web/src/app/dashboard/schedule/components/schedule-calendar.tsx
@@ -4,17 +4,12 @@ import { api } from "@dev-team-fall-25/server/convex/_generated/api";
 import { FunctionReturnType } from "convex/server";
 
 export interface ScheduleCalendarProps {
-  classes:
-    | FunctionReturnType<typeof api.userCourseOfferings.getUserCourseOfferings>
-    | undefined;
+  classes: FunctionReturnType<
+    typeof api.userCourseOfferings.getUserCourseOfferings
+  >;
 }
 
 export function ScheduleCalendar({ classes }: ScheduleCalendarProps) {
-  if (!classes) {
-    // TODO: add loading state here
-    return;
-  }
-
   // TODO: implement the component to display all the classes
   return <></>;
 }
