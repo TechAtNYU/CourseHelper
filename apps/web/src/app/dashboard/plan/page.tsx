87be4f47c0ae645c6ce2383ba013b3c3cfc792c7
Chenxin Yan <yanchenxin2004@gmail.com>
2025-10-17 17:11:57 -0400

    update plan page to fetch preloaded courses

--- apps/web/src/app/dashboard/plan/page.tsx
+++ apps/web/src/app/dashboard/plan/page.tsx
@@ -1,7 +1,17 @@
-import FourYearPlan from "./components/FourYearPlan";
+import { api } from "@dev-team-fall-25/server/convex/_generated/api";
+import { preloadQuery } from "convex/nextjs";
+import { getAuthToken } from "@/lib/convex";
+import PlanContent from "./components/plan-content";
 
-const PlanPage = () => {
-  return <FourYearPlan />;
+const PlanPage = async () => {
+  const token = await getAuthToken();
+  const preloadedCourses = await preloadQuery(
+    api.userCourses.getUserCourses,
+    {},
+    { token },
+  );
+
+  return <PlanContent preloadedCourses={preloadedCourses} />;
 };
 
 export default PlanPage;
