edef155293b13ea069f25173fb5705e332b956a1
Michael Wang <mzw2010@nyu.edu>
2025-10-18 19:31:51 -0400

    imported shadcn chart skeleton

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,7 +1,13 @@
 import FileUploadButton from "@/modules/report-parsing/components/file-upload-button";
+import { ChartBarMixed } from "./components/charts/degree-charts";
 
 const HomePage = () => {
-  return <FileUploadButton />;
+  return (
+    <div className="space-y-6 p-6">
+      <FileUploadButton />
+      <ChartBarMixed />
+    </div>
+  );
 };
 
 export default HomePage;
