de42c2c3b8db93490d01f61a44dce511ad4c4367
James Shin <js13820@nyu.edu>
2025-10-19 16:43:48 -0400

    Update drizzle-dev.config.ts

--- apps/scraper/drizzle-dev.config.ts
+++ apps/scraper/drizzle-dev.config.ts
@@ -4,11 +4,7 @@ import { defineConfig } from "drizzle-kit";
 config({ path: "./.db.env" });
 
 export default defineConfig({
-  out: "./src/drizzle/migrations",
   schema: "./src/drizzle/schema.ts",
+  out: "./src/drizzle/migrations",
   dialect: "sqlite",
-  dbCredentials: {
-    // biome-ignore lint/style/noNonNullAssertion: env variables must exist
-    url: process.env.DEV_DATABASE_URL!,
-  },
 });
