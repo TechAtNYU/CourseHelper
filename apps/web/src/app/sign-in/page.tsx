02c695ac17a9657eae121178cffcb2eed129fb2b
Chenxin Yan <yanchenxin2004@gmail.com>
2025-10-19 15:02:24 -0400

    clean up

--- apps/web/src/app/sign-in/page.tsx
+++ apps/web/src/app/sign-in/page.tsx
@@ -1,9 +1,16 @@
-import { LoginForm } from "@/app/sign-in/components/login-form";
+import { GalleryVerticalEnd } from "lucide-react";
+import { LoginForm } from "./components/login-form";
 
 export default function LoginPage() {
   return (
-    <div className="bg-muted relative flex min-h-svh items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <a href="/" className="flex items-center gap-2 self-center font-medium">
+          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+            <GalleryVerticalEnd className="size-4" />
+          </div>
+          CourseHelper
+        </a>
         <LoginForm />
       </div>
     </div>
