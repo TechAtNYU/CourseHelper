da0ce2721967a552fb6975dc53f846dc676e72c6
xyspg <kangjm2005@hotmail.com>
2025-10-26 04:40:34 -0400

    feat(web): add dnd upload report (#51)
    Co-authored-by: Chenxin Yan <yanchenxin2004@gmail.com>

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
-import FileUploadButton from "@/modules/report-parsing/components/file-upload-button";
+import DegreeProgreeUpload from "@/modules/report-parsing/components/degree-progress-upload";
 
 const HomePage = () => {
-  return <FileUploadButton />;
+  return <DegreeProgreeUpload />;
 };
 
 export default HomePage;
