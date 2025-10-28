e75256c7dfdac557cbf2c981a5ab8f61fcc47445
Chenxin Yan <yanchenxin2004@gmail.com>
2025-10-27 22:58:34 -0400

    replace hardcoded course filtering term and year

--- apps/web/src/app/dashboard/schedule/components/schedule-content.tsx
+++ apps/web/src/app/dashboard/schedule/components/schedule-content.tsx
@@ -3,6 +3,11 @@
 import type { api } from "@albert-plus/server/convex/_generated/api";
 import { type Preloaded, usePreloadedQuery } from "convex/react";
 import type { FunctionReturnType } from "convex/server";
+import {
+  type Term,
+  useCurrentTerm,
+  useCurrentYear,
+} from "@/components/AppConfigProvider";
 import { ScheduleCalendar } from "./schedule-calendar";
 
 interface ScheduleContentProps {
@@ -15,20 +20,25 @@ function getUserClassesByTerm(
   classes: FunctionReturnType<
     typeof api.userCourseOfferings.getUserCourseOfferings
   >,
-  year: number,
-  term: string,
+  year: number | null,
+  term: Term | null,
 ) {
+  if (!year || !term) {
+    return [];
+  }
   return classes.filter((cls) => {
     return cls.courseOffering.year === year && cls.courseOffering.term === term;
   });
 }
 
 export function ScheduleContent({ preloadedClasses }: ScheduleContentProps) {
-  // TODO: hardcoded for now, need to use app configs
+  const currentYear = useCurrentYear();
+  const currentTerm = useCurrentTerm();
+
   const classes = getUserClassesByTerm(
     usePreloadedQuery(preloadedClasses),
-    2025,
-    "fall",
+    currentYear,
+    currentTerm,
   );
 
   return <ScheduleCalendar classes={classes} />;
