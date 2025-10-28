508933940a4a8f38375e5caa9800c0dc595730cc
Chenxin Yan <yanchenxin2004@gmail.com>
2025-10-28 02:27:11 -0400

    add app header take in center component

--- apps/web/src/app/dashboard/components/app-header.tsx
+++ apps/web/src/app/dashboard/components/app-header.tsx
@@ -1,7 +1,13 @@
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 
-export function AppHeader({ title }: { title: string }) {
+export function AppHeader({
+  title,
+  center,
+}: {
+  title: string;
+  center?: React.ReactNode;
+}) {
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-2 px-4">
@@ -11,6 +17,7 @@ export function AppHeader({ title }: { title: string }) {
           className="data-[orientation=vertical]:h-4 mr-2"
         />
         <h1 className="text-base font-medium">{title}</h1>
+        {center && <div className="flex-1 flex justify-center">{center}</div>}
       </div>
     </header>
   );
