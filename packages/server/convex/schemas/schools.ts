8076fd13a4773c3495aacf1327a963d559bf7de7
Chenxin Yan <yanchenxin2004@gmail.com>
2025-10-30 02:09:39 -0400

    add level

--- packages/server/convex/schemas/schools.ts
+++ packages/server/convex/schemas/schools.ts
@@ -3,4 +3,5 @@ import { v } from "convex/values";
 export const schools = {
   name: v.string(), // College of Arts and Science
   shortName: v.string(), // CAS
+  level: v.union(v.literal("undergraduate"), v.literal("graduate")),
 };
