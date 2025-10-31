e7e5ca4dce55918c16bd589c40556a47d576abdd
Chenxin Yan <yanchenxin2004@gmail.com>
2025-10-31 06:02:49 -0400

    add metadata

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -5,16 +5,25 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://docs.albertplus.com",
   integrations: [
     starlight({
-      title: "My Docs",
+      title: "AlbertPlus Docs",
       social: [
         {
           icon: "github",
           label: "GitHub",
-          href: "https://github.com/withastro/starlight",
+          href: "https://github.com/TechAtNYU/AlbertPlus",
+        },
+        {
+          icon: "discord",
+          label: "Discord",
+          href: "https://discord.gg/z8p4Qtjatw",
         },
       ],
+      editLink: {
+        baseUrl: "https://github.com/TechAtNYU/AlbertPlus/edit/main/apps/docs",
+      },
       sidebar: [
         {
           label: "Guides",
